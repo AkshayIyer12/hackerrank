58500697cc5a372f8acfd34e881b52dee123d3c1
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-25 21:34:15 +0530

    Add jumping on the clouds revision

---
+++ jumping_on_the_clouds_revisited.js
@@ -0,0 +1,36 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    c = readLine().split(' ');
+    c = c.map(Number);
+    let thundercloud = 0
+    for (let i = 0; i < n; i++) {
+      let t = c[i]
+      if ((i % k === 0) && t === 1) thundercloud++
+    }
+  console.log(100 - (n/k) - (2 * thundercloud))
+
+}
