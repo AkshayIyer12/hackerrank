49dfe77eaf7a4eba5ea0f3441a7f7f2ddb8fb7d5
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-25 21:09:32 +0530

    Add birthday bar prob

---
+++ the_birthday_bar.js
@@ -0,0 +1,45 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function solve(n, s, d, m){
+    // Complete this function
+  let count = 0
+  for (let i = 0; i < n; i++) {
+    let newArr = s.slice(i, m+i)
+    let sum = newArr.reduce((a, v) => a+v, 0)
+    if (sum === d) count++
+    else continue
+  }
+  return count
+}
+
+function main() {
+    var n = parseInt(readLine());
+    s = readLine().split(' ');
+    s = s.map(Number);
+    var d_temp = readLine().split(' ');
+    var d = parseInt(d_temp[0]);
+    var m = parseInt(d_temp[1]);
+    var result = solve(n, s, d, m);
+    process.stdout.write(""+result+"\n");
+
+}
