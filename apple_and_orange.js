e0801b4798f48a8d6ce8921583954d1ed03f43e1
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-25 21:05:03 +0530

    Add apple and orange prob

---
+++ apple_and_orange.js
@@ -0,0 +1,48 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s_temp = readLine().split(' ');
+    var s = parseInt(s_temp[0]);
+    var t = parseInt(s_temp[1]);
+    var a_temp = readLine().split(' ');
+    var a = parseInt(a_temp[0]);
+    var b = parseInt(a_temp[1]);
+    var m_temp = readLine().split(' ');
+    var m = parseInt(m_temp[0]);
+    var n = parseInt(m_temp[1]);
+    apple = readLine().split(' ');
+    apple = apple.map(Number);
+    orange = readLine().split(' ');
+    orange = orange.map(Number);
+    let newApple = apple.map(value => a+value)
+    let newOrange = orange.map(value => b+value)
+    let newApp = newApple.filter(a => {
+      if(a>=s && a<=t) return a
+    })
+    let newOr = newOrange.filter(o => {
+      if(o>=s && o<=t) return o
+    })
+    console.log(newApp.length)
+    console.log(newOr.length)
+
+}
